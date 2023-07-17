e55b5b8cd0e6036940246e209fc241d0db4661b5
Alex Wilkins <86133577+calvirick@users.noreply.github.com>
2023-07-17 15:37:29 -0700

    Add TabLinks and state persistence between routes

---
+++ src/lib/components/TabLink.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+
+	export let url: string = '/';
+	export let color: string = 'bg-white';
+</script>
+
+<li>
+	<a
+		href={url}
+		class={$page.url.pathname === url
+			? `${color} font-bold text-black`
+			: ` focus:${color} active:${color} hover:font-bold hover:text-black hover:${color}`}
+	>
+		<slot />
+	</a>
+</li>
+
+<style lang="postcss">
+	.tab-link {
+	}
+</style>
